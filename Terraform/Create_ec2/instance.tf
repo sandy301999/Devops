40553748d533cf03fc3a63f4cc5434941a9a8d36
sandy301999 <61046327+sandy301999@users.noreply.github.com>
2025-06-29 15:27:31 +0530

    Update instance.tf

--- Terraform/Create_ec2/instance.tf
+++ Terraform/Create_ec2/instance.tf
@@ -1,1 +1,12 @@
 
+	resource "aws_instance" "web" {
+	  ami                    = data.aws_ami.amiID.id
+	  instance_type          = "t3.micro"
+	  key_name               = aws_key_pair.sample-key.key_name
+	  vpc_security_group_ids = [aws_security_group.dove-sg.id]
+	  availability_zone      = "us-east-1a"
+	  tags = {
+	    Name    = "dove-web"
+	    Project = "Dove"
+	  }
+}
