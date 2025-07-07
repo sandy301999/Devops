68f624e31d4eb4852ade267ec7f3cb83106bafb0
sandy301999 <61046327+sandy301999@users.noreply.github.com>
2025-07-07 17:03:00 +0530

    Update main.tf

--- infra-pipeline/terraform/main.tf
+++ infra-pipeline/terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-05ffe3c48a9991133"
   instance_type = "t2.micro"
   key_name      = "kops-key"
 
