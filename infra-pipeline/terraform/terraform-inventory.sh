e6c190654329060025efc9795296a20341e1f396
sandy301999 <61046327+sandy301999@users.noreply.github.com>
2025-07-07 17:46:54 +0530

    Update terraform-inventory.sh

--- infra-pipeline/terraform/terraform-inventory.sh
+++ infra-pipeline/terraform/terraform-inventory.sh
@@ -5,5 +5,5 @@ PUBLIC_IP=$(terraform output -raw public_ip)
 
 cat <<EOF > inventory.ini
 [web]
-$PUBLIC_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/kops-key.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+$PUBLIC_IP ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/kops-key.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
