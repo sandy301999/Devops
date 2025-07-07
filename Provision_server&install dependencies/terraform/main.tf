f19f6d89a8c63c517b540b43036208cb49a4daea
sandy301999 <61046327+sandy301999@users.noreply.github.com>
2025-07-07 16:17:38 +0530

    Add files via upload

---
+++ Provision_server&install dependencies/terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
---
+++ Provision_server&install dependencies/terraform/terraform-inventory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+terraform output -raw public_ip > public_ip.txt
+PUBLIC_IP=$(cat public_ip.txt)
+
+cat <<EOF > inventory.ini
+[web]
+$PUBLIC_IP ansible_user=ubuntu ansible_ssh_private_key_file=../keys/devops-key.pem
+EOF
